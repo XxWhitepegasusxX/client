8676d78b7d68ec94a87c9bc8cf14afe7279fa1b1
Alexander Almeida <alexanderalmeida3040@gmail.com>
2022-06-21 21:10:47 -0300

    consertando bug / fixing bug

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from './App'
-import { ContextProvider } from "./SocketContext";
+import App from './App';
+import { ContextProvider } from './SocketContext';
+
 import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <ContextProvider>
-            <App/>
-        </ContextProvider>
-    </React.StrictMode>
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById('root'),
 );
-
